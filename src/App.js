3e0560b3aea94368c44ddc4eaf335af4362a4819
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-12-09 22:16:45 +0600

    removing files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 
@@ -12,24 +11,3 @@ const App = () => (
 );
 
 export default App;
-
-// import CreateToDo from './components/CreateToDo';
-// import ShowToDos from './components/ShowToDos';
-  // const [value, setValue] = React.useState('hello');
-  // const [todos, setTodos] = React.useState([]);
-  // function onFormSubmit (e) {
-  //   e.preventDefault();
-  //   setTodos([...todos, value]);
-  //   setValue('');
-  // }
-  // function onInputChange (e) {
-  //   setValue(e.target.value);
-  // }
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <h1>my to do list</h1>
-    //     <CreateToDo value={value} onFormSubmit={onFormSubmit} onInputChange={onInputChange}  />
-    //     <ShowToDos todos={todos} />
-    //   </header>
-    // </div>
