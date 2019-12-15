a7554fac2540f89b02186bf210d5acbd4dfc9593
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-12-15 13:46:52 +0600

    navbar setup

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'emotion-theming';
 import theme from './helpers/theme';
 import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Navbar from './components/organisms/Navbar';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <ThemeProvider theme={theme}>
-        <Route path="/" component={Home} exact={true} />
-      </ThemeProvider>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Switch>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/about" component={About} exact={true} />
+      </Switch>
+    </ThemeProvider>
   </BrowserRouter>
 );
 
