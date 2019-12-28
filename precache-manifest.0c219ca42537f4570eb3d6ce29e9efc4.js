4714688ac498c1e448cf8d1753df2b33af5a8184
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-12-28 14:54:13 +0600

    Updates

---
+++ precache-manifest.0c219ca42537f4570eb3d6ce29e9efc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b651b016550ae9ddaebfce04bcb4145a",
+    "url": "/maker-space/index.html"
+  },
+  {
+    "revision": "08084c50313d24d787d7",
+    "url": "/maker-space/static/js/2.cc75f482.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/maker-space/static/js/2.cc75f482.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acb19b51c0ebcc124794",
+    "url": "/maker-space/static/js/main.7ea38da6.chunk.js"
+  },
+  {
+    "revision": "b7e1ef730bf1b6c27815",
+    "url": "/maker-space/static/js/runtime-main.8f716122.js"
+  }
+]);
